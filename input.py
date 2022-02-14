9feb6b1bde31ac6e38be035107781c12ad09b926
Monty van Emmerik <mvanemmerik@me.com>
2022-02-14 06:43:19 -0500

    add input

---
+++ input.py
@@ -0,0 +1,5 @@
+name  = input("What is your name? ")
+birthdate = input("What is your birthdate? ")
+age = int(input("How old are you? "))
+
+print(f"{name} was born on {birthdate} and is {age} years old.")
