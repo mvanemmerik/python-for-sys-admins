9a6fb2668968bd98d308ae5045f172fa28662d66
Monty van Emmerik <mvanemmerik@me.com>
2022-02-15 07:17:50 -0500

    add error handling and exit code

--- reverse_file.py
+++ reverse_file.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 # build thhe parser
 parser = argparse.ArgumentParser(description='Read a file in reverse')
@@ -7,14 +8,21 @@ parser.add_argument('--limit', '-l', type=int, help='the number of  lines to rea
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 args  = parser.parse_args()
 
-with  open(args.filename) as f:
-    lines = f.readlines()
-    lines.reverse() # reverse the lines, first line is now last line
-    
-    if args.limit:
-        lines = lines[:args.limit]
-    
-    for line in  lines:
-        print(line.strip()[::-1]) # reverse the conent of each line with a '-1' step
-
-
+try:
+    f = open(args.filename)
+    limit =  args.limit
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+    sys.exit(2)
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse() # reverse the lines, first line is now last line
+        
+        if args.limit:
+            lines = lines[:limit]
+        
+        for line in  lines:
+            print(line.strip()[::-1]) # reverse the conent of each line with a '-1' step
+finally:
+    print("---")
