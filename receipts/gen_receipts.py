d6ff7b6d44578b5e35310f4a9a5af6258cacfceb
Monty van Emmerik <mvanemmerik@me.com>
2022-02-15 21:31:41 -0500

    random and json: receipt generator

---
+++ receipts/gen_receipts.py
@@ -0,0 +1,17 @@
+import random
+import json
+import os
+
+count = int(os.getenv("FILE_COUNT") or 10)
+words = [word.strip() for word in open('/usr/share/dict/words').readlines()]
+
+for identifier in range(count):
+    amount = random.uniform(1.0, 1000)
+    content = {
+        'topic': random.choice(words),
+        'value': "%.2f" % amount
+    }
+    with open(f'./new/receipt-{identifier}.json', 'w') as f:
+        json.dump(content, f)
+
+# FILE_COUNT=25 python gen_receipts.py
